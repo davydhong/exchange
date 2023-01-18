5be64f4016d81a3d7aa36cbda59ab178c4ce3217
David Hong <davydhong@gmail.com>
2023-01-18 10:16:05 -0800

    css

--- src/CurrencyForm.tsx
+++ src/CurrencyForm.tsx
@@ -1,36 +1,77 @@
 import React, { useState } from 'react';
 import { ExchangeRate } from './exchangeRates';
+import styled from 'styled-components';
 
 interface Props {
     exchangeRates: ExchangeRate[];
     onConvert: (amount: number, selectedCurrency: ExchangeRate) => void;
 }
 
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const StyledLabel = styled.label`
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+`;
+
+const StyledInput = styled.input`
+  padding: 10px;
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  
+`;
+
+const StyledSelect = styled.select`
+  padding: 10px;
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  
+`;
+
+const StyledButton = styled.button`
+  padding: 10px 20px;
+  background-color: #4CAF50;
+  color: white;
+  font-size: 1.2rem;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #3e8e41;
+  }
+`;
+
 const CurrencyForm: React.FC<Props> = ({ exchangeRates, onConvert }) => {
     const [amount, setAmount] = useState<number>(0);
     const [selectedCurrency, setSelectedCurrency] = useState<ExchangeRate>(exchangeRates[0]);
 
     return (
-        <form>
-            <label>
+        <StyledForm>
+            <StyledLabel>
                 Amount in CZK:
-                <input type="number" value={amount} onChange={e => setAmount(parseFloat(e.target.value))} />
-            </label>
-            <br />
-            <label>
+                <StyledInput type="number" value={amount} onChange={e => setAmount(parseFloat(e.target.value))} />
+            </StyledLabel>
+            <StyledLabel>
                 Select Currency:
-                <select value={selectedCurrency.code} onChange={e => setSelectedCurrency(exchangeRates.find(rate => rate.code === e.target.value))}>
+                <StyledSelect value={selectedCurrency.code} onChange={e => setSelectedCurrency(exchangeRates.find(rate => rate.code === e.target.value))}>
                     <option value="" disabled>Select a currency</option>
                     {exchangeRates.map(rate => (
                         <option key={rate.code} value={rate.code}>{rate.code}</option>
                     ))}
-                </select>
-            </label>
-            <br />
-            <button onClick={() => onConvert(amount, selectedCurrency)}>Convert</button>
+                </StyledSelect>
+            </StyledLabel>
+            <StyledButton onClick={() => onConvert(amount, selectedCurrency)}>Convert</StyledButton>
             <br />
             {selectedCurrency && <p>{amount} CZK is equal to {(amount * selectedCurrency.rate).toFixed(2)} {selectedCurrency.code}</p>}
-        </form >
+        </StyledForm >
     );
 }
 
--- src/CurrencyTable.tsx
+++ src/CurrencyTable.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { ExchangeRate } from './exchangeRates';
 
-
 type CurrencyTableProps = {
     currencyData: ExchangeRate[];
 }
@@ -10,40 +9,52 @@ type CurrencyTableProps = {
 const CurrencyTable: React.FC<CurrencyTableProps> = ({ currencyData }: CurrencyTableProps) => {
 
     return (
-        <Table>
+        <StyledTable>
             <thead>
                 <tr>
-                    <th>Country</th>
-                    <th>Currency</th>
-                    <th>Amount</th>
-                    <th>Code</th>
-                    <th>Rate</th>
+                    <StyledTh>Country</StyledTh>
+                    <StyledTh>Currency</StyledTh>
+                    <StyledTh>Amount</StyledTh>
+                    <StyledTh>Code</StyledTh>
+                    <StyledTh>Rate</StyledTh>
                 </tr>
             </thead>
             <tbody>
                 {currencyData.map((data) => (
-                    <tr key={data.code}>
-                        <td>{data.country}</td>
-                        <td>{data.currency}</td>
-                        <td>{data.amount}</td>
-                        <td>{data.code}</td>
-                        <td>{data.rate}</td>
-                    </tr>
+                    <StyledTr key={data.code}>
+                        <StyledTd>{data.country}</StyledTd>
+                        <StyledTd>{data.currency}</StyledTd>
+                        <StyledTd>{data.amount}</StyledTd>
+                        <StyledTd>{data.code}</StyledTd>
+                        <StyledTd>{data.rate}</StyledTd>
+                    </StyledTr>
                 ))}
             </tbody>
-        </Table>
+        </StyledTable>
     );
 };
 
-const Table = styled.table`
+const StyledTable = styled.table`
     width: 100%;
     border-collapse: collapse;
-    thead {
-        font-weight: bold;
-    }
-    th, td {
-        border: 1px solid #ddd;
-        padding: 8px;
+    margin: 30px 0;
+`;
+
+const StyledTh = styled.th`
+    font-weight: bold;
+    text-align: left;
+    padding: 12px;
+    background-color: #f5f5f5;
+`;
+
+const StyledTd = styled.td`
+    padding: 12px;
+    border-bottom: 1px solid #ddd;
+`;
+
+const StyledTr = styled.tr`
+    &:hover {
+        background-color: #f5f5f5;
     }
 `;
 
